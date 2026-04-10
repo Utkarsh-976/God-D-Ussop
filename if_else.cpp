e52b71601545cc53cccf590c5c2b707fc2040af6
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-04-10 20:53:24 +0530

    Add files via upload

--- if_else.cpp
+++ if_else.cpp
@@ -1,28 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-//if else or if else ladder
-int main()
-{
-    int age;
-    cout << "Tell me your age" << endl;
-    cin >> age;
 
-    if ((age < 18) && (age > 0))
-    {
-        cout << "You cannot drink Alcohol";
+int main(){
+    int a = 3;
+    int b = a+1;
+
+    if((a = 3)==b){
+        cout<<a;
     }
-    else if (age == 18)
-    {
-        cout << "you just attend party but not drink anything";
-    }
-    else if (age < 1)
-    {
-        cout << "Go back!";
-    }
-    else
-    {
-        cout << "You can drink";
-    }
+        else{
+            cout<<a+1;
+        }
 
     return 0;
 }
