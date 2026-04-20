4721fdb89ed77f0e1de00746ec03a6703b2bfab8
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-04-20 22:17:04 +0530

    Add files via upload

---
+++ Pattern_ABCD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    char start = 'A';
+    while(row <= n)
+    {
+        int col = 1;
+
+        while(col <= n)
+        {        
+            cout << start <<" ";
+            col = col + 1;
+            start = start + 1;
+          }
+        cout << endl;
+        row = row + 1;
+    }
+
+ }
