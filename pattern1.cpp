8d012c89c5aa64aba07184c4598fdc5713f5c476
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-07 22:24:41 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_pattern1(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern2(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<=i; j++){
+            cout<<"* ";
+
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern3(int n){
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<j;
+
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern4(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern5(int n){
+    for(int i = 1; i<=n; i++){
+        for (int j = 0; j < n-i+1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern6(int n){
+    for(int i = 1; i<=n; i++){
+        for (int j = 1; j <= n-i+1; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"input: ";
+    cin>>n;
+    print_pattern1(n);  
+    print_pattern2(n); 
+    print_pattern3(n);
+    print_pattern4(n);
+    print_pattern5(n);
+    print_pattern6(n);
+
+    return 0;
+}
