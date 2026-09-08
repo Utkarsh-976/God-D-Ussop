2cc579a4516a2aee0313cc2d492c95f756697786
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-08 23:20:27 +0530

    Create recursionpower.cpp

---
+++ recursionpower.cpp
@@ -0,0 +1,14 @@
+// 1. Power of a number
+#include <iostream>
+using namespace std;
+
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+
+    return a * power(a, b - 1);
+}
+
+int main() {
+    cout << power(2, 4);   // 16
+}
