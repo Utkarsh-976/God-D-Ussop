51fb9593678613d21ec33e935045032b0f6ffd9a
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-08 23:17:48 +0530

    Create recursion2.cpp

---
+++ recursion2.cpp
@@ -0,0 +1,14 @@
+// 1. Factorial
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main() {
+    cout << factorial(5);
+}
